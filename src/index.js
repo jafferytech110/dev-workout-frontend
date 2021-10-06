9cbaa682508e907a48325b6a9d331e97b6b99f36
jafferytech110 <sarfarazalijaffery@gmail.com>
2021-10-06 22:15:30 +0200

    major changes in file sizes and index.js code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import _ from "lodash";
 import "./style.css";
-import "./iStock-1144.jpg";
-import "./iStock-1157.jpg";
+import "./iStock-1144-min.jpg";
+import "./iStock-1157-min.jpg";
 
 //slideshow
 
@@ -18,22 +18,14 @@ let count = 1;
 
 //images are stored in an array
 let images = [];
-images[0] = "./iStock-1144.jpg";
-images[1] = "./iStock-1157.jpg";
-
-//getting h1 of slider
-const sliderh1 = document.querySelector(".slider-h1");
-let text = ["we prefer pug over bug", "webpack for code packing"];
-
-//setting initial image
-slider.style.backgroundImage = `url(images/${images[0]})`;
+images[0] = "./iStock-1144-min.jpg";
+images[1] = "./iStock-1157-min.jpg";
 
 //using setInterval function to keep calling slidingImage function after some duration
 setInterval(slidingImage, timeDuration);
 
 function slidingImage() {
   slider.style.backgroundImage = `url(images/${images[count]})`;
-  sliderh1.textContent = text[count];
   if (count < images.length - 1) {
     count++;
   } else {
